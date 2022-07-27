3bbb40380727b0b6cdbcec0bbc6c196dbf1c9956
maba-dev <cherif.ba@hotmail.fr>
2022-07-27 02:43:48 +0000

    a function that calculates the shape of a matrix

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,11 @@
+def matrix_shape(matrix):
+    """ a function that calculates the shape of a matrix: """
+
+
+    if type(matrix) is not list:
+        return None
+    shape = []
+    while (type(matrix) is list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
