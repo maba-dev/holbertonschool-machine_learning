f6471cdc415ec77a42c44f7aeb0df1326f63626f
maba-dev <cherif.ba@hotmail.fr>
2022-07-28 13:46:07 +0000

    a function that transposes matrix

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""  a function that transposes matrix"""
+
+
+import numpy as np
+
+
+def np_transpose(matrix):
+    """transposes matrix """
+    return np.transpose(matrix)
