2038763c48219cfee9550cd8be950a8b14fefd1f
maba-dev <cherif.ba@hotmail.fr>
2022-07-28 00:03:27 +0000

    a function that adds two matrices element-wise

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -9,7 +9,7 @@ def add_matrices2D(mat1, mat2):
     result = []
     for i in range(len(mat1)):
         line_mat = []
-        for j in range(len(mat1)):
+        for j in range(len(mat1[0])):
             line_mat.append(mat1[i][j] + mat2[i][j])
         result.append(line_mat)
     return result
