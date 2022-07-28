cd967c286c1009e81da2cd6d05addba870e90246
maba-dev <cherif.ba@hotmail.fr>
2022-07-28 00:00:00 +0000

    a function that adds two matrices element-wise

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,7 +4,7 @@
 
 def add_matrices2D(mat1, mat2):
     """adds two matrices element-wise"""
-    if (len(mat1) != len(mat2)) or (len(mat1[0]) != len(mat2[0])):
+    if (len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0])):
         return None
     result = []
     for i in range(len(mat1)):
