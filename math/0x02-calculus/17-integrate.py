908e48cb623c779ed544787f572e053fb482dbe5
maba-dev <cherif.ba@hotmail.fr>
2022-08-04 18:05:43 +0000

    a function that calculates the integral of a polynomial

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""a function that calculates the integral of a polynomial """
+
+
+def poly_integral(poly, C=0):
+    """calculates the integral of a polynomia """
+    if type(poly) is not list:
+        return None
+    if len(poly) == 1:
+        return [C]
+    if len(poly) == 0:
+        return None
+    if type(C) is not int:
+        return None
+    result = []
+    result.append(C)
+    for i in range(0, len(poly)):
+        if type(poly[i]) is not int:
+            return None
+        coef = poly[i] / (i + 1)
+        if coef.is_integer():
+            result.append(int(coef))
+        else:
+            result.append(coef)
+    return result
