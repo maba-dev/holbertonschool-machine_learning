0b41c4ac6e20bd8249928d692511ad96607c36ff
maba-dev <cherif.ba@hotmail.fr>
2022-08-10 08:28:33 +0000

    Calculates the value of the PMF for a given number of “successes”

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -16,3 +16,15 @@ class Poisson:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = sum(data) / len(data)
+
+    def pmf(self, k):
+        """Calculates the value of the PMF for a given number of “successes”"""
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        factoriel_k = 1
+        e = 2.7182818285
+        for i in range(1, k + 1):
+            factoriel_k = factoriel_k * i
+        return ((self.lambtha ** k) * (e ** (-(self.lambtha))) / factoriel_k)
