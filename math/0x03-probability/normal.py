616f5b897a3894381b2e036e9850bf055cb4c302
maba-dev <cherif.ba@hotmail.fr>
2022-08-10 13:18:11 +0000

    a class Normal that represents a normal distribution

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" a class Normal that represents a normal distribution"""
+
+
+from typing import Type
+
+
+class Normal:
+    """a class normal distribution """
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """ constructor """
+        self.stddev = float(stddev)
+        self.mean = float(mean)
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple")
+            self.mean = sum(data) / len(data)
+            _data = []
+            N = 1 / len(data)
+            for i in data:
+                _data.append((i - self.mean) ** 2)
+            self.stddev = (N * sum(_data)) ** (1 / 2)
