f963496dfeed1507d8b8066a69569fbf8e4252ef
maba-dev <cherif.ba@hotmail.fr>
2022-08-10 09:15:50 +0000

    a class Exponential that represents an exponential distribution

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" a class Exponential that represents an exponential distribution """
+
+
+class Exponential:
+    """ a class an exponential distribution """
+    def __init__(self, data=None, lambtha=1.):
+        """ constructor"""
+        self.lambtha = float(lambtha)
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = 1 / (sum(data) / len(data))
