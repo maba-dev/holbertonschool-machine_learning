2227da86f78bfc68dcc704d1c172c85bfe809f1c
maba-dev <cherif.ba@hotmail.fr>
2022-08-14 20:11:06 +0000

    a class Binomial that represents a binomial distribution:

---
+++ math/0x03-probability/binomial.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""a class Binomial that represents a binomial distribution """
+
+
+class Binomial:
+    """a class Binomial distribution  """
+    def __init__(self, data=None, n=1, p=0.5):
+        """constructor"""
+        self.n = int(n)
+        self.p = float(p)
+        if data is None:
+            if n < 1:
+                raise ValueError("n must be a positive value")
+            if p >= 1 or p <= 0:
+                raise ValueError("p must be greater than 0 and less than 1")
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            mean = sum(data) / len(data)
+            N = 1 / len(data)
+            _data = []
+            for i in data:
+                _data.append((i - mean) ** 2)
+            variance = sum(_data) * N
+            self.p = 1 - (variance) / (mean)
+            self.n = int(round(mean / self.p))
+            self.p = mean / self.n
