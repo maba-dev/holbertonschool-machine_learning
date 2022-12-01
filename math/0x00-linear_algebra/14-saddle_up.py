d006611a39ad7964f041f04d7cc8b22096c9d76d
maba-dev <cherif.ba@hotmail.fr>
2022-12-01 09:11:23 +0000

    a function that performs matrix multiplication

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""a function that performs matrix multiplication"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """  matrix multiplication"""
+    return np.matmul(mat1, mat2)
