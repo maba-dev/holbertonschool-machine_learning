860bac347961e63dbd07715934cc107cc1112e56
maba-dev <cherif.ba@hotmail.fr>
2023-01-02 11:28:34 +0000

    retry task 0

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 import numpy as np
+"""
+    a class Neuron that defines a single neuron
+    performing binary classification
+"""
 
 
 class Neuron():
-    """
-        a class Neuron that defines a single neuron
-        performing binary classification
-    """
+    """ define the class"""
+
     def __init__(self, nx):
         """ class constructor"""
 
         if type(nx) is not int:
             raise TypeError('nx must be an integer')
-        if nx <= 0:
+        if nx < 1:
             raise ValueError('nx must be a positive integer')
         self.nx = nx
         self.W = np.random.rand(1, nx)
