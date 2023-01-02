2f5215e9f3507656a7edcbdfeb82fb35ecc8e3cc
maba-dev <cherif.ba@hotmail.fr>
2023-01-02 11:45:45 +0000

    correct the random function

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -14,9 +14,9 @@ class Neuron():
     def __init__(self, nx):
         """ class constructor"""
         if type(nx) is not int:
-            raise TypeError('nx must be an integer')
+            raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError('nx must be a positive integer')
-        self.W = np.random.rand(1, nx)
+            raise ValueError("nx must be a positive integer")
+        self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
