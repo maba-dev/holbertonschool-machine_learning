9b58f6ec0dfdf917d4b5cdea398e35ac4b550e41
maba-dev <cherif.ba@hotmail.fr>
2023-01-02 11:33:47 +0000

    correct pycodestyle

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-import numpy as np
 """
     a class Neuron that defines a single neuron
     performing binary classification
 """
+
+
+import numpy as np
 
 
 class Neuron():
@@ -11,12 +13,10 @@ class Neuron():
 
     def __init__(self, nx):
         """ class constructor"""
-
         if type(nx) is not int:
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        self.nx = nx
         self.W = np.random.rand(1, nx)
         self.b = 0
         self.A = 0
