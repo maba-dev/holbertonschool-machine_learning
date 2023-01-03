0fcb352868e70c94067ba59c3e941c8bbc4753d3
maba-dev <cherif.ba@hotmail.fr>
2023-01-03 09:54:22 +0000

    methode Calculates the forward propagation of the neuron

---
+++ supervised_learning/0x01-classification/2-neuron.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+    a class Neuron that defines a single neuron
+    performing binary classification
+"""
+
+
+import numpy as np
+
+
+class Neuron():
+    """ define the class"""
+
+    def __init__(self, nx):
+        """ class constructor"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        """ Calculates the forward propagation of the neuron """
+        z = np.matmul(self.__W, X) + self.b
+        self.__A = (1 / (1 + np.exp(-z)))
+        return self.__A
