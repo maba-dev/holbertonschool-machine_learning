64308017bb63e67f086f00d1422c2ed67c3e00d2
maba-dev <cherif.ba@hotmail.fr>
2023-01-17 20:47:06 +0100

    calculates the specificity for each class in a confusion matrix:

---
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""calculates the specificity for each class in a confusion matrix: """
+
+
+import numpy as np
+
+
+def precision(confusion):
+    """define the function """
+    TP = np.diag(confusion)
+    FP = np.sum(confusion, axis=0) - TP
+    PPV = TP / (TP + FP)
+    return PPV
