0743e4248ee6fd7c46706f1f534212fba7626a18
maba-dev <cherif.ba@hotmail.fr>
2023-01-17 20:51:41 +0100

    correct the betty

--- supervised_learning/0x04-error_analysis/2-precision.py
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""calculates the specificity for each class in a confusion matrix: """
+"""that calculates the precision for each class in a confusion matrix """
 
 
 import numpy as np
--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" """
+""" that calculates the specificity for each class in a confusion matrix"""
 
 
 import numpy as np
 
 
 def specificity(confusion):
-    """ """
+    """ define the function"""
     TP = np.diag(confusion)
     FN = np.sum(confusion, axis=1) - TP
     FP = np.sum(confusion, axis=0) - TP
