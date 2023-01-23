3a154dea6a767f5d0b43475c95514bcf184542ef
maba-dev <cherif.ba@hotmail.fr>
2023-01-24 00:54:27 +0100

    Calculates accuracy of prediction

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Calculates accuracy of prediction"""
+"""Calculates accuracy of prediction"""
 
 
 import tensorflow as tf
@@ -7,8 +7,6 @@ import tensorflow as tf
 
 def calculate_accuracy(y, y_pred):
     """define the function"""
-    pred = tf.math.argmax(y_pred, axis=1)
-    ny = tf.math.argmax(y, axis=1)
-    equality = tf.math.equal(pred, ny)
-    accuracy = tf.math.reduce_mean(tf.cast(equality, tf.float32))
+    correct_preduction = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
+    accuracy = tf.math.reduce_mean(tf.cast(correct_preduction, tf.float32))
     return accuracy
