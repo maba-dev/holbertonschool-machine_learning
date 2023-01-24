d9e583854585b74a4c2508a4677dcf632bb92786
maba-dev <cherif.ba@hotmail.fr>
2023-01-24 21:31:13 +0100

    evaluates the output of a neural network

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" evaluates the output of a neural network"""
+
+import tensorflow.compat.v1 as tf
+
+
+def evaluate(X, Y, save_path):
+    """ define the function"""
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph("{}.meta".format(save_path))
+        saver.restore(sess, save_path)
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
+        eval_y_pred = sess.run(y_pred, feed_dict={x: X, y: Y})
+        eval_accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
+        eval_loss = sess.run(loss, feed_dict={x: X, y: Y})
+        return eval_y_pred, eval_accuracy, eval_loss
