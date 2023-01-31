cd8bbdc72e9addd558f05dec0a97907d2bb43410
maba-dev <cherif.ba@hotmail.fr>
2023-01-31 23:22:56 +0100

    calculates the weighted moving average of a data set:

--- supervised_learning/0x03-optimization/4-moving_average.py
+++ supervised_learning/0x03-optimization/4-moving_average.py
@@ -6,12 +6,7 @@ def moving_average(data, beta):
     """calculates the weighted moving average of a data set:"""
     mv_averages = []
     avg = 0
-    bias_correction = 1
-    for i, value in enumerate(data):
-        avg = beta * avg + (1 - beta) * value
-        bias_correction = bias_correction * beta
-        if i + 1 == len(data):
-            mv_averages.append(avg / (1 - bias_correction))
-        else:
-            mv_averages.append(avg / (1 - bias_correction))
+    for i in range(len(data)):
+        avg = (beta * avg) + (data[i] * (1 - beta))
+        mv_averages.append(avg / (1 - beta**(i + 1)))
     return mv_averages
