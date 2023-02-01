b9be7dabf52bc1f109cef6e12b6c8a55857b3d6c
maba-dev <cherif.ba@hotmail.fr>
2023-02-01 01:12:17 +0100

    creates the training operation for a neural network in     tensorflow using the RMSProp optimization algorithm:

---
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    creates the training operation for a neural network in 
+    tensorflow using the RMSProp optimization algorithm:
+"""
+
+
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """
+        creates the training operation for a neural network in 
+        tensorflow using the RMSProp optimization algorithm:
+    """
+    optimizer = tf.optimizers.RMSprop(alpha, beta2, epsilon=epsilon)
+    return optimizer.minimize(loss)
