c265fd2c6f32d79793e64f1fc96363aa53cdcc41
maba-dev <cherif.ba@hotmail.fr>
2023-02-01 03:44:11 +0100

    normalizes an unactivated output of a neural network using batch normalization

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-"""normalizes an unactivated output of a neural network using batch normalization """
-
+""" normalizes an unactivated output of a neural
+    network using batch normalization
+"""
 
 
 import numpy as np
 
 
 def batch_norm(Z, gamma, beta, epsilon):
-    """normalizes an unactivated output of a neural network using batch normalization """
+    """ normalizes an unactivated output of a neural
+        network using batch normalization
+    """
     mean = np.mean(Z, axis=0)
     var = np.var(Z, axis=0)
     Z_norm = (Z - mean) / np.sqrt(var + epsilon)
