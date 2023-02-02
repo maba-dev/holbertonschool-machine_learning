2b0b64c00846bddea9e0de3a20b208dc9ddbf23e
maba-dev <cherif.ba@hotmail.fr>
2023-02-02 03:54:58 +0100

    calculates the cost of a neural network with L2 regularization

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -7,4 +7,4 @@ import tensorflow.compat.v1 as tf
 
 def l2_reg_cost(cost):
     """calculates the cost of a neural network with L2 regularization"""
-    return cost + tf.losses.get_regularization_loss()
+    return cost + tf.losses.get_regularization_losses()
