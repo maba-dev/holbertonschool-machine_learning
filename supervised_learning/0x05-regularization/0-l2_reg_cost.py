be53ad6e5f5df6b81457c72d0b934e0f56e617a6
maba-dev <cherif.ba@hotmail.fr>
2023-02-02 02:34:05 +0100

    calculates the cost of a neural network with L2 regularization:

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -8,7 +8,7 @@ import numpy as np
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """ calculates the cost of a neural network with L2 regularization:"""
     reg_cost = 0
-    for i in range(1, L):
+    for i in range(1, L + 1):
         reg_cost += np.sum(np.square(weights["W" + str(i)]))
     reg_cost = (lambtha / (2 * m)) * reg_cost
     return cost + reg_cost
