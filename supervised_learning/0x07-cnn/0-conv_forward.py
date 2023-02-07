f82e48663f2f04c15b83eb903c2f50570372101a
maba-dev <cherif.ba@hotmail.fr>
2023-02-07 21:32:27 +0100

    performs forward propagation over a convolutional layer of a neural network:

---
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" """
+
+
+import numpy as np
+
+
+def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
+    """ """
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw, c_prev, c_new = W.shape
+    sh, sw = stride
+    
+    if padding == "same":
+        ph = (h_prev - 1) * sh + kh - h_prev
+        pw = (w_prev - 1) * sw + kw - w_prev
+        A_prev = np.pad(A_prev, ((0,0), (ph//2, ph//2), (pw//2, pw//2), (0,0)), 'constant')
+    else:
+        ph, pw = 0, 0
+    
+    h = int((h_prev + 2 * ph - kh) / sh + 1)
+    w = int((w_prev + 2 * pw - kw) / sw + 1)
+    
+    Z = np.zeros((m, h, w, c_new))
+    for i in range(h):
+        for j in range(w):
+            for k in range(c_new):
+                start_h = i * sh
+                end_h = start_h + kh
+                start_w = j * sw
+                end_w = start_w + kw
+                Z[:, i, j, k] = np.sum(A_prev[:, start_h:end_h, start_w:end_w, :] * W[:,:,:,k], axis=(1,2,3)) + b[0,0,0,k]
+    
+    A = activation(Z)
+    cache = (A_prev, W, b, stride, padding)
+    
+    return A, cache
