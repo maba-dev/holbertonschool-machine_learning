81d8a10676c58b6d59795aa4371e14b89cc90f78
maba-dev <cherif.ba@hotmail.fr>
2023-02-09 00:21:11 +0100

    performs forward propagation over a convolutional layer of a neural network

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -4,29 +4,32 @@
 
 import numpy as np
 
+
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
-    """ performs forward propagation over a convolutional layer of a neural network:"""
-    m, h_prev, w_prev, c_prev = A_prev.shape
-    kh, kw, c_prev, c_new = W.shape
-    sh, sw = stride
+    """performs forward propagation over a convolutional layer of a neural network"""
     
-    if padding == "same":
-        ph = int((h_prev - 1) * sh + kh - h_prev), 0
-        pw = int((w_prev - 1) * sw + kw - w_prev), 0
-        A_prev = np.pad(A_prev, ((0, 0), (ph//2, ph//2), (pw//2, pw//2), (0, 0)), "constant")
-    h_prev, w_prev = A_prev.shape[1], A_prev.shape[2]
-    
-    h = int(1 + (h_prev - kh) / sh)
-    w = int(1 + (w_prev - kw) / sw)
-    conv = np.zeros((m, h, w, c_new))
-    
-    for i in range(h):
-        for j in range(w):
-            x = i * sh
-            y = j * sw
-            a_slice = A_prev[:, x:x+kh, y:y+kw, :]
-            for k in range(c_new):
-                conv[:, i, j, k] = np.sum(a_slice * W[:, :, :, k], axis=(1, 2, 3))
-                
-    Z = conv + b
-    return Z
+    if padding == 'same':
+        ph = int(((A_prev.shape[1] - 1) * stride[0] +
+                  W.shape[0] - A_prev.shape[1]) / 2)
+        pw = int(((A_prev.shape[2] - 1) * stride[1] +
+                  W.shape[1] - A_prev.shape[2]) / 2)
+    else:
+        ph = 0
+        pw = 0
+    nh = int(((A_prev.shape[1] - W.shape[0] + (2 * ph)) / stride[0]) + 1)
+    nw = int(((A_prev.shape[2] - W.shape[1] + (2 * pw)) / stride[1]) + 1)
+    conv = np.zeros((A_prev.shape[0], nh, nw, W.shape[3]))
+    npad = ((0, 0), (ph, ph), (pw, pw), (0, 0))
+    A_pad = np.pad(A_prev, pad_width=npad,
+                   mode='constant', constant_values=0)
+    for i in range(nh):
+        x = i * stride[0]
+        for j in range(nw):
+            y = j * stride[1]
+            for k in range(W.shape[3]):
+                A = A_pad[:, x:x + W.shape[0],
+                          y:y + W.shape[1], :]
+                kernel = W[:, :, :, k]
+                conv[:, i, j, k] = np.sum(np.multiply(A, kernel),
+                                               axis=(1, 2, 3))
+    return activation(conv + b)
