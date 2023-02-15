898e647953844bd1a14afbd05d0b604b8f6afa51
maba-dev <cherif.ba@hotmail.fr>
2023-02-15 01:19:45 +0100

    builds a neural network with the Keras library

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env python3
 """builds a neural network with the Keras library """
 
-
-from keras.layers import Input, Dense, Dropout
-from keras.models import Model
-from keras import regularizers
-
+import tensorflow.keras as K
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """builds a neural network with the Keras library"""
-    x_input = Input(shape=(nx,))
+    x_input = K.layers.Input(shape=(nx,))
 
     prev_layer = x_input
     for i in range(len(layers)):
         layer_size = layers[i]
         activation = activations[i]
-        layer = Dense(
+        layer = K.layers.Dense(
             layer_size, activation=activation,
-            kernel_regularizer=regularizers.l2(lambtha))(prev_layer)
+            kernel_regularizer=K.regularizers.l2(lambtha))(prev_layer)
         if keep_prob < 1.0:
-            layer = Dropout(1 - keep_prob)(layer)
+            layer = K.layers.Dropout(1 - keep_prob)(layer)
         prev_layer = layer
-    model = Model(inputs=x_input, outputs=prev_layer)
+    model = K.models.Model(inputs=x_input, outputs=prev_layer)
     return model
