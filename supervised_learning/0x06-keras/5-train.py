286529f48b81e021f6ba23323e11841f449c7a28
maba-dev <cherif.ba@hotmail.fr>
2023-02-16 01:57:58 +0100

    validate the model

---
+++ supervised_learning/0x06-keras/5-train.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""  validate the model """
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """ validate the model  """
+    History = network.fit(x=data, y=labels, batch_size=batch_size,
+                          epochs=epochs, verbose=verbose, shuffle=shuffle,
+                          validation_data=validation_data)
+    return History
