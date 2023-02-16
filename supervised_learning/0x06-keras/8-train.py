b6a9e9bb0bf66f0d56bd9592c97d6d56d613f8ae
maba-dev <cherif.ba@hotmail.fr>
2023-02-16 03:12:11 +0100

    save the best iteration of the model:

---
+++ supervised_learning/0x06-keras/8-train.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" save the best iteration of the model:"""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1, decay_rate=1,
+                save_best=False, filepath=None, verbose=True, shuffle=False):
+    """ save the best iteration of the model:"""
+    def learning_rate_decay(epoch):
+        return alpha / (1 + decay_rate * epoch)
+
+    callbacks = []
+    if validation_data:
+        early_stop = K.callbacks.EarlyStopping(monitor='val_loss',
+                                               patience=patience,
+                                               mode='min')
+        learning = K.callbacks.LearningRateScheduler(
+            schedule=learning_rate_decay, verbose=1
+        )
+        callbacks.append(learning)
+    if filepath:
+        callbacks.append(K.callbacks.ModelCheckpoint(filepath,
+                                                     save_best_only=save_best,
+                                                     monitor='val_loss'))
+    history = network.fit(data, labels,
+                          batch_size=batch_size,
+                          epochs=epochs, verbose=verbose,
+                          callbacks=callbacks,
+                          validation_data=validation_data,
+                          shuffle=shuffle)
+    return history
