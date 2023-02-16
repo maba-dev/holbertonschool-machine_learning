b1734de208bb0c22d3f95ad0e269a2374884244e
maba-dev <cherif.ba@hotmail.fr>
2023-02-16 01:44:40 +0100

    trains a model using mini-batch gradient descent

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -2,13 +2,13 @@
 """trains a model using mini-batch gradient descent"""
 
 
-from tensorflow import keras
+import tensorflow.keras as K
 
 
 def train_model(network,
                 data, labels, batch_size, epochs, verbose=True, shuffle=False):
     """trains a model using mini-batch gradient descent:"""
-    model = keras.models.clone_model(network)
+    model = K.models.clone_model(network)
     model.compile(
         optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
     num_samples = data.shape[0]
