7594cdc13e2b7d1430babafb287336c14ea4fc8f
maba-dev <cherif.ba@hotmail.fr>
2023-02-16 01:51:44 +0100

    trains a model using mini-batch gradient descent

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -7,15 +7,14 @@ import tensorflow.keras as K
 
 def train_model(network,
                 data, labels, batch_size, epochs, verbose=True, shuffle=False):
-    """trains a model using mini-batch gradient descent:"""
+    """trains a model using mini-batch gradient descent"""
 
-    History = model.fit(
+    History = network.fit(
         data,
         labels,
         batch_size=batch_size,
         epochs=epochs,
         verbose=verbose,
-        steps_per_epoch=steps_per_epoch,
         shuffle=shuffle
     )
     return History
