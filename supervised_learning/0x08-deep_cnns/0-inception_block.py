87d353a5f1b961099a4ef1a0353bb407af0187f5
maba-dev <cherif.ba@hotmail.fr>
2023-02-19 23:40:00 +0100

    builds an inception block as described in Going Deeper with Convolutions (2014):

--- supervised_learning/0x08-deep_cnns/0-inception_block.py
+++ supervised_learning/0x08-deep_cnns/0-inception_block.py
@@ -1,25 +1,32 @@
 #!/usr/bin/env python3
-"""builds an inception block as described in Going Deeper with Convolutions (2014):"""
+"""builds an inception block as described in Going Deeper with Convolutions"""
+
 
 import tensorflow.keras as K
 
 
 def inception_block(A_prev, filters):
-    """ builds an inception block as described in Going Deeper with Convolutions (2014):"""
+    """build an inception block as described in Going Deep with Convolution"""
     F1, F3R, F3, F5R, F5, FPP = filters
     conv1x1 = K.layers.Conv2D(
-        filters=F1, kernel_size=(1, 1), padding='same', activation='relu')(A_prev)
+        filters=F1,
+        kernel_size=(1, 1), padding='same', activation='relu')(A_prev)
     conv3x3 = K.layers.Conv2D(
-        filters=F3R, kernel_size=(1, 1), padding='same', activation='relu')(A_prev)
+        filters=F3R,
+        kernel_size=(1, 1), padding='same', activation='relu')(A_prev)
     conv3x3 = K.layers.Conv2D(
-        filters=F3, kernel_size=(3, 3), padding='same', activation='relu')(conv3x3)
+        filters=F3,
+        kernel_size=(3, 3), padding='same', activation='relu')(conv3x3)
     conv5x5 = K.layers.Conv2D(
-        filters=F5R, kernel_size=(1, 1), padding='same', activation='relu')(A_prev)
+        filters=F5R,
+        kernel_size=(1, 1), padding='same', activation='relu')(A_prev)
     conv5x5 = K.layers.Conv2D(
-        filters=F5, kernel_size=(5, 5), padding='same', activation='relu')(conv5x5)
+        filters=F5,
+        kernel_size=(5, 5), padding='same', activation='relu')(conv5x5)
     maxpool = K.layers.MaxPooling2D(
         pool_size=(3, 3), strides=(1, 1), padding='same')(A_prev)
     maxpool = K.layers.Conv2D(
-        filters=FPP, kernel_size=(1, 1), padding='same', activation='relu')(maxpool)
+        filters=FPP,
+        kernel_size=(1, 1), padding='same', activation='relu')(maxpool)
     output = K.layers.concatenate([conv1x1, conv3x3, conv5x5, maxpool])
     return output
