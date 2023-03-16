71538bbf36708a810772ece9a0c2650c87165121
maba-dev <cherif.ba@hotmail.fr>
2023-03-16 16:11:41 +0100

    calculates a correlation matrix

---
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""  calculates a correlation matrix"""
+
+
+import numpy as np
+
+
+def correlation(C):
+    """ calculates a correlation matrix """
+    if not isinstance(C, np.ndarray):
+        raise TypeError("C must be a numpy.ndarray")
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
+        raise ValueError("C must be a 2D square matrix")
+    d = C.shape[0]
+    corr = np.zeros((d, d))
+    for i in range(d):
+        for j in range(d):
+            corr[i, j] = C[i, j] / np.sqrt(C[i, i] * C[j, j])
+    return corr
